e4a2254ad4bcaa4e21fd2a2f450a2649f7419729
myuon <ioi.joi.koi.loi@gmail.com>
2018-09-12 21:37:38 +0900

    perf: fast problem 10

--- src/problem10.hs
+++ src/problem10.hs
@@ -1,8 +1,26 @@
-import qualified Data.Vector.Unboxed as V
+import Control.Monad
+import Data.IORef
+import qualified Data.Vector.Unboxed.Mutable as V
 
 bound = 2000000
-primes = 2 : 3 : [n | i <- [1..], j <- [1,-1], let n = 6*i+j, all (\p -> n `mod` p /= 0) $ takeWhile (\p -> p * p <= n) primes]
 
--- 15.2s
-main = print $ sum $ takeWhile (<= bound) primes
+primeArray :: IO (V.IOVector Bool)
+primeArray = do
+  vec <- V.replicate bound False
+  forM_ (takeWhile (\n -> n * n <= bound) [2..bound]) $ \n -> do
+    forM_ (takeWhile (< bound) $ map (*n) $ tail [1..]) $ \i -> V.write vec i True
+
+  return vec
+
+solve :: IO Int
+solve = do
+  sum <- newIORef 0
+  vec <- primeArray
+  forM_ [2..bound-1] $ \i -> do
+    r <- V.read vec i
+    when (not r) $ modifyIORef sum (+i)
+
+  readIORef sum
+
+main = print =<< solve
 
